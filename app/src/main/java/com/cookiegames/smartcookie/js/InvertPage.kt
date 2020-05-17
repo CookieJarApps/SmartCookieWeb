a4434bd01543d53c094c20ee235357cf1a7ac446
CookieGamesOfficial <support@cookiegames.co.uk>
2020-05-17 12:01:21 +0100

    Multiline tab page

--- app/src/main/java/com/cookiegames/smartcookie/js/InvertPage.kt
+++ app/src/main/java/com/cookiegames/smartcookie/js/InvertPage.kt
@@ -2,9 +2,7 @@ package com.cookiegames.smartcookie.js
 
 import com.anthonycr.mezzanine.FileStream
 
-/**
- * Invert the color of the page.
- */
+
 @FileStream("app/src/main/js/InvertPage.js")
 interface InvertPage {
 
