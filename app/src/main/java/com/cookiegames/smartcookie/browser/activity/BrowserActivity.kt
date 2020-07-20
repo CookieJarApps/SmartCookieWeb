24495d7de284e8422cc5e7a60078c4fb56636703
CookieGamesOfficial <support@cookiegames.co.uk>
2020-07-20 19:52:31 +0100

    Bookmark imports no longer need a restart

--- app/src/main/java/com/cookiegames/smartcookie/browser/activity/BrowserActivity.kt
+++ app/src/main/java/com/cookiegames/smartcookie/browser/activity/BrowserActivity.kt
@@ -1339,6 +1339,8 @@ abstract class BrowserActivity : ThemableBrowserActivity(), BrowserView, UIContr
         }
 
         changeToolbarColor(null)
+
+        handleBookmarksChange()
     }
 
     /**
