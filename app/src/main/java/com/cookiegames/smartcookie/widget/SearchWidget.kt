02488835555ad1de510030c75d0a7bc4c67ed39d
CookieGamesOfficial <support@cookiegames.co.uk>
2020-10-26 12:00:15 +0000

    Started adding widget, fixed homepage bug

---
+++ app/src/main/java/com/cookiegames/smartcookie/widget/SearchWidget.kt
@@ -0,0 +1,44 @@
+package com.cookiegames.smartcookie.widget
+
+import android.app.PendingIntent
+import android.app.SearchManager
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+import android.widget.SearchView
+import androidx.core.content.ContextCompat.getSystemService
+import com.cookiegames.smartcookie.MainActivity
+import com.cookiegames.smartcookie.R
+
+class SearchWidget : AppWidgetProvider() {
+
+    override fun onUpdate(
+            context: Context,
+            appWidgetManager: AppWidgetManager,
+            appWidgetIds: IntArray
+    ) {
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        appWidgetIds.forEach { appWidgetId ->
+            // Create an Intent to launch ExampleActivity
+            val pendingIntent: PendingIntent = Intent(context, MainActivity::class.java)
+                    .let { intent ->
+                        PendingIntent.getActivity(context, 0, intent, 0)
+                    }
+
+            // Get the layout for the App Widget and attach an on-click listener
+            // to the button
+            val views: RemoteViews = RemoteViews(
+                    context.packageName,
+                    R.layout.search_widget
+            ).apply {
+                //setOnClickPendingIntent(R.id.search_widget_button, pendingIntent)
+            }
+
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
