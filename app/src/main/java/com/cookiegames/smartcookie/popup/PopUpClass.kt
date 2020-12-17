c05db7541f815e30057ad5caec541a4c137ed911
CookieGamesOfficial <support@cookiegames.co.uk>
2020-12-17 13:23:00 +0000

    Added new license notices

--- app/src/main/java/com/cookiegames/smartcookie/popup/PopUpClass.kt
+++ app/src/main/java/com/cookiegames/smartcookie/popup/PopUpClass.kt
@@ -1,4 +1,8 @@
-// Copyright 2020 CookieJarApps MPL
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Created by CookieJarApps 7/11/2020 */
+
 package com.cookiegames.smartcookie.popup
 
 import android.app.Activity
