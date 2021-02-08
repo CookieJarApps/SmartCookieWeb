87937b73d22e4900dce52272b3f49097ac8aab30
CookieGamesOfficial <support@cookiegames.co.uk>
2021-02-08 12:57:47 +0000

    Incognito page improvements

--- app/src/main/java/com/cookiegames/smartcookie/html/incognito/IncognitoPageFactory.kt
+++ app/src/main/java/com/cookiegames/smartcookie/html/incognito/IncognitoPageFactory.kt
@@ -3,7 +3,11 @@ package com.cookiegames.smartcookie.html.incognito
 import android.app.Application
 import android.content.Context
 import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.util.Base64
 import android.util.Log
+import android.webkit.URLUtil
 import com.cookiegames.smartcookie.AppTheme
 import com.cookiegames.smartcookie.R
 import com.cookiegames.smartcookie.constant.FILE
@@ -12,14 +16,18 @@ import com.cookiegames.smartcookie.database.history.HistoryDatabase
 import com.cookiegames.smartcookie.database.history.HistoryRepository
 import com.cookiegames.smartcookie.html.HtmlPageFactory
 import com.cookiegames.smartcookie.html.ListPageReader
+import com.cookiegames.smartcookie.html.homepage.HomePageFactory
 import com.cookiegames.smartcookie.html.homepage.HomePageReader
 import com.cookiegames.smartcookie.html.jsoup.*
 import com.cookiegames.smartcookie.preference.UserPreferences
 import com.cookiegames.smartcookie.search.SearchEngineProvider
+import com.cookiegames.smartcookie.utils.DrawableUtils
 import dagger.Reusable
 import io.reactivex.Single
+import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileWriter
+import java.net.URL
 import javax.inject.Inject
 
 /**
@@ -59,17 +67,32 @@ class IncognitoPageFactory @Inject constructor(
                         id("title-pm"){text(resources.getString(R.string.private_title))}
                         id("desc-pm"){text(resources.getString(R.string.private_desc))}
                         if(userPreferences.showShortcuts){
+                            var shortcuts = arrayListOf(userPreferences.link1, userPreferences.link2, userPreferences.link3, userPreferences.link4)
+
                             id("edit_shortcuts"){ text(resources.getString(R.string.edit_shortcuts)) }
                             id("apply"){ text(resources.getString(R.string.apply)) }
-                            id("link1click"){ attr("href", userPreferences.link1)}
-                            id("link2click"){ attr("href", userPreferences.link2)}
-                            id("link3click"){ attr("href", userPreferences.link3)}
-                            id("link4click"){ attr("href", userPreferences.link4)}
-                            id("link1"){ attr("src", userPreferences.link1 + "/favicon.ico")}
-                            id("link2"){ attr("src", userPreferences.link2 + "/favicon.ico")}
-                            id("link3"){ attr("src", userPreferences.link3 + "/favicon.ico")}
-                            id("link4"){ attr("src", userPreferences.link4 + "/favicon.ico")}
-                            id("search_input"){ attr("placeholder", resources.getString(R.string.search_homepage))}
+                            id("link1click"){ attr("href", shortcuts[0])}
+                            id("link2click"){ attr("href", shortcuts[1])}
+                            id("link3click"){ attr("href", shortcuts[2])}
+                            id("link4click"){ attr("href", shortcuts[3])}
+
+                            shortcuts.forEachIndexed { index, element ->
+                                if(!URLUtil.isValidUrl(element)){
+                                    val icon = createIconByName('?')
+                                    val encoded = bitmapToBase64(icon)
+
+                                    id("link" + (index + 1)){ attr("src", "data:image/png;base64," + encoded)}
+
+                                    return@forEachIndexed
+                                }
+
+                                val url = URL(element.replaceFirst("www.", ""))
+                                val icon = createIconByName(url.getHost().first().toUpperCase())
+                                val encoded = bitmapToBase64(icon)
+                                id("link" + (index + 1)){ attr("src", element + "/favicon.ico")}
+                                id("link" + (index + 1)){ attr("onerror", "this.src = 'data:image/png;base64,$encoded';")}
+
+                            }
                         }
                         else{
                             id("shortcuts"){ attr("style", "display: none;")}
@@ -108,9 +131,27 @@ class IncognitoPageFactory @Inject constructor(
             .map { (page, _) -> "$FILE$page" }
 
     /**
-     * Create the home page file.
+     * Create the incognito page file.
      */
     fun createIncognitoPage() = File(application.filesDir, FILENAME)
+
+    fun createIconByName(name: Char): Bitmap {
+        val icon = DrawableUtils.createRoundedLetterImage(
+                name,
+                64,
+                64,
+                Color.GRAY
+        )
+        return icon
+    }
+
+    fun bitmapToBase64(bitmap: Bitmap): String{
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 80, byteArrayOutputStream)
+        val byteArray: ByteArray = byteArrayOutputStream.toByteArray()
+        val encoded: String = Base64.encodeToString(byteArray, Base64.NO_WRAP)
+        return encoded
+    }
 
     companion object {
 
